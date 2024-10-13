5ff185ef336ab330ab7c6fa1ba0703f7e0b13ec4
LennonSouza <lennonsouza2009@gmail.com>
2024-10-13 12:31:50 -0300

    Adicionar arquivos de projeto.

---
+++ Data/DbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Patsy.Data;
+
+public class DbContext : IdentityDbContext
+{
+    public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Patsy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
